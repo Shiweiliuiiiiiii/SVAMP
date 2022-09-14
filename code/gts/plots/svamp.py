c3aabaa7058dee781de76291aeb9e7b770d92aa8
刘世伟 <liushiwei@v093020.vpn.tue.nl>
2022-09-14 10:32:49 -0500

    svamp omp before

--- code/gts/plots/svamp.py
+++ code/gts/plots/svamp.py
@@ -18,11 +18,14 @@ dense = [41.2]
 
 
 # robert_gm_before =     [23.1, 26.2, 24.7, 27.0, 25.6, 24.6, 24.7, 24.1, 22.9, 21.9]
-robert_random_before = [23.1, 26.2, 24.7, 27.0, 25.6, 24.6, 24.7, 24.1, 22.9, 21.9]
-robert_random_rigl =   [28.4, 21.5, 25.3, 27.5, 26.2, 24.7, 25.0, 23.3, 24.0, 22.6]
 robert_snip_rigl =     [27.6, 27.5, 27.6, 26.2, 25.8, 26.0, 26.2, 25.7, 25.4, 24.8]
 robert_snip =          [26.9, 26.6, 27.4, 26.2, 26.0, 25.6, 26.4, 25.8, 25.1, 26.2]
 robert_lth =           [39.8, 40.1, 39.1, 35.7, 33.7, 25.6, 24.4, 23.6, 22.3, 21.3]
+
+
+robert_random_before = [23.1, 26.2, 24.7, 27.0, 25.6, 24.6, 24.7, 24.1, 22.9, 21.9]
+robert_random_after  = [25.2, 21.4, 25.5, 25.7, 25.6, 25.3, 25.3, 24.2, 23.0, 22.6]
+robert_random_rigl =   [28.4, 21.5, 25.3, 27.5, 26.2, 24.7, 25.0, 23.3, 24.0, 22.6]
 
 x_axis = range(10)
 
@@ -37,7 +40,7 @@ roberta_large.plot(x_axis, robert_lth,  '-',   label='LTH',color='orange',linewi
 # roberta_large.plot(x_axis, robert_gm_after,  '--',   label='OMP After',color='green',linewidth=linewidth, markersize=markersize, )
 # roberta_large.plot(x_axis, robert_gm_rigl,  '-',   label='OMP+RIGL',color='green',linewidth=linewidth, markersize=markersize, marker='^' )
 roberta_large.plot(x_axis, robert_random_before,  '-',   label='Random Before',color='purple',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, robert_random_after,  '--',   label='Random After',color='purple',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_random_after,  '--',   label='Random After',color='purple',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, robert_random_rigl,  '-',   label='Random+RIGL',color='purple',linewidth=linewidth, markersize=markersize, marker='^' )
 
 
