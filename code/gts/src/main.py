88e95ffd8852dd2a90833e133ecb3e9ca16531c6
刘世伟 <liushiwei@v092252.vpn.tue.nl>
2022-09-15 12:51:40 -0500

    clean historical info for gmp

--- code/gts/src/main.py
+++ code/gts/src/main.py
@@ -303,6 +303,18 @@ def main():
 
 				logger.debug('Training for epoch {} completed...\nTime Taken: {}'.format(epoch, time_since(time.time() - start)))
 
+
+				if mask and args.sparse_mode == 'GMP' and mask.steps >= mask.final_prune_time:
+					logger.info('clean historical metrics for GMP')
+					max_value_corr = 0
+					len_total_eval = 0
+					max_val_acc = 0.0
+					max_train_acc = 0.0
+					eq_acc = 0.0
+					best_epoch = -1
+					min_train_loss = float('inf')
+
+
 				if loss_total / len(input_lengths) < min_train_loss:
 					min_train_loss = loss_total / len(input_lengths)
 
