11d7f339d36d2794503cbb7a3f8f95150f7a590c
刘世伟 <liushiwei@v092252.vpn.tue.nl>
2022-09-15 18:43:57 -0500

    adjust plot size

--- code/gts/plots/overall.py
+++ code/gts/plots/overall.py
@@ -214,7 +214,7 @@ roberta_large.spines['top'].set_visible(False)
 
 # plt.tight_layout()
 fig.legend(loc='lower center', bbox_to_anchor=(0.0, 0.0, 1, 1), fancybox=False, shadow=False, ncol=4, fontsize=fontsize, frameon=False)
-fig.subplots_adjust(left=0.02 , bottom=0.2, right=0.98, top=0.95, wspace=0.15, hspace=0.2)
+fig.subplots_adjust(left=0.02 , bottom=0.22, right=0.98, top=0.95, wspace=0.15, hspace=0.2)
 
 plt.savefig('gts_no_omp_before.pdf')
 plt.show()
