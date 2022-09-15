5a764ca9982c79f21c46a8c47a91d843a3835d54
刘世伟 <liushiwei@v092252.vpn.tue.nl>
2022-09-15 18:44:50 -0500

    adjust plot size

--- code/gts/plots/overall.py
+++ code/gts/plots/overall.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os, re
 from matplotlib.pyplot import figure
-fig = figure(num=None, figsize=(19, 5.5), dpi=150, facecolor='w', edgecolor='k')
+fig = figure(num=None, figsize=(19, 6), dpi=150, facecolor='w', edgecolor='k')
 fontsize = 14
 Titlesize = 18
 markersize = 5
@@ -214,7 +214,7 @@ roberta_large.spines['top'].set_visible(False)
 
 # plt.tight_layout()
 fig.legend(loc='lower center', bbox_to_anchor=(0.0, 0.0, 1, 1), fancybox=False, shadow=False, ncol=4, fontsize=fontsize, frameon=False)
-fig.subplots_adjust(left=0.02 , bottom=0.22, right=0.98, top=0.95, wspace=0.15, hspace=0.2)
+fig.subplots_adjust(left=0.02 , bottom=0.3, right=0.98, top=0.95, wspace=0.2, hspace=0.2)
 
 plt.savefig('gts_no_omp_before.pdf')
 plt.show()
