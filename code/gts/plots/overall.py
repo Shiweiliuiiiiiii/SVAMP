576c77fb74a43447d13ba1f5af2eabb42eaef338
刘世伟 <liushiwei@v097211.vpn.tue.nl>
2022-09-18 15:01:31 -0500

    add labels for y

--- code/gts/plots/overall.py
+++ code/gts/plots/overall.py
@@ -16,86 +16,6 @@ def sorted_nicely(l):
 
 
 
-# asdiv 
-
-dense_asdiv = [78.64]
-
-results_dir = "/home/sliu/project_space/pruning_cfails/Math/gts/asdiv/"
-output_files = os.listdir(os.path.join(results_dir))
-all_methods = sorted_nicely(output_files)
-
-
-results_sdiv = []
-for method in all_methods:
-    if 'dense' not in method and 'imp' not in method:
-        print(method)
-        method_dirs = os.path.join(results_dir, str(method))
-        method_sparsity = sorted_nicely(os.listdir(method_dirs))
-
-        for sparsity in method_sparsity:
-            print(sparsity)
-            sparsity_dir = os.path.join(method_dirs, sparsity,'out', 'CV_results_cv_asdiv-a.json')
-            with open(sparsity_dir) as file:
-                for line in file:
-                    if '5-fold avg acc score' in line:
-                        results_sdiv.append(100*float(line.split()[-1][1:-2]))
-
-print(np.array(results_sdiv).reshape(10,-1))
-robert_gmp_asdiv = [77.24, 78.55, 78.23, 75.35, 69.60, 65.57, 66.56, 67.63, 67.94, 67.79]
-robert_gm_after_asdiv = [79.78, 78.96466721, 79.21117502, 68.69350863, 65.73541495, 68.44700082, 67.78964667,  67.4609696, 65.16023007, 63.92769104]
-robert_gm_before_asdiv = [78.14297453, 71.73377157,  0.82169269,  0.82169269,  0.82169269,  0.82169269, 0.82169269 , 0.82169269 , 0.82169269 , 0.82169269]
-robert_gm_rigl_asdiv = [75.51, 68.69, 0.82169269,  0.82169269,  0.82169269,  0.82169269, 0.82169269 , 0.82169269 , 0.82169269 , 0.82169269]
-
-robert_random_after_asdiv = [71.73377157, 67.4609696,  62.53081348, 63.43467543, 60.14790468, 60.55875103,
-  59.90139688, 58.75102712, 57.27198028, 58.50451931]
-
-# print(robert_random_after_asdiv)
-robert_random_before_asdiv = [48.06902219, 45.76828266, 51.68447001, 54.97124076, 59.16187346, 59.07970419,
-  58.91536565, 58.17584224, 55.87510271, 45.35743632]
-# print(robert_random_before_asdiv)
-robert_random_rigl_asdiv = [35.08627773 ,44.28923583, 49.30156122, 56.86113394, 55.05341002, 58.09367297,
-  57.60065735, 59.98356615, 56.9433032,  53.73870173]
-robert_snip_asdiv= [59.65488907, 56.20377979, 61.05176664 ,60.39441249 ,53.73870173 ,60.55875103,
-  60.47658176, 59.49055053, 59.81922761, 60.31224322 ]
-robert_snip_rigl_asdiv= [60.06573541, 61.38044371, 61.70912079, 61.13393591, 61.21610518, 60.72308956,
-  61.13393591 ,61.46261298,60.6409203 , 59.49055053 ]
-
-robert_lth_asdiv = [80.27, 79.38, 77.32, 71.90, 65.08, 59.57, 56.86, 54.15, 54.81, 52.67]
-
-
-
-x_axis = range(10)
-roberta_large = fig.add_subplot(1,3,2)
-roberta_large.plot(x_axis, dense_asdiv*10,  '-o',   label='Dense model',color='black',linewidth=linewidth, markersize=markersize, )
-
-roberta_large.plot(x_axis, robert_snip_asdiv,  '-o',   label='SNIP (Before)',color='#00FF00',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_lth_asdiv,  '-o',   label='LTH (After)',color='orange',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_snip_rigl_asdiv,  '--o',   label='SNIP+RIGL (Before)',color='#00FF00',linewidth=linewidth, markersize=markersize )
-roberta_large.plot(x_axis, robert_gm_after_asdiv,  '-o',   label='One-Shot LRR (After)',color='#00BFFF',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_random_before_asdiv,  '-o',   label='Random (Before)',color='brown',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_random_after_asdiv,  '-o',   label='Random LRR (After)',color='#0072BD',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_random_rigl_asdiv,  '--o',   label='Random+RIGL (Before)',color='brown',linewidth=linewidth, markersize=markersize)
-roberta_large.plot(x_axis, robert_gmp_asdiv,  '-o',   label='GMP (During)',color='magenta',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, robert_gm_before_asdiv,  '-o',   label='OMP (Before)' ,color='#bcbd22',linewidth=linewidth, markersize=markersize, )
-# roberta_large.plot(x_axis, robert_gm_rigl_asdiv,  '--o',   label='OMP+RIGL (Before)',color='#bcbd22',linewidth=linewidth, markersize=markersize )
-
-
-roberta_large.set_title('GTS on ASDiv-A',fontsize=Titlesize)
-roberta_large.set_xticks(range(10))
-# roberta_large.axes.get_xaxis().set_visible(False)
-roberta_large.set_ylabel('Accuracy [%]', fontsize=Titlesize)
-roberta_large.set_xlabel('Sparsity',fontsize=fontsize)
-roberta_large.set_xticklabels([], rotation=45,fontsize=fontsize )
-roberta_large.set_xticklabels(np.array( [0.2, 0.36, 0.488, 0.590, 0.672, 0.738, 0.791, 0.8325, 0.866, 0.893]), fontsize=10 )
-roberta_large.tick_params(axis='both', which='major', labelsize=fontsize)
-# xposition = [3,7]
-# for xc in xposition:
-#     plt.axvline(x=xc, color='#a90308', linestyle='--', alpha=0.5)
-roberta_large.grid(True, linestyle='-', linewidth=0.5, )
-
-roberta_large.spines['right'].set_visible(False)
-roberta_large.spines['top'].set_visible(False)
-
 
 # mawps
 
@@ -184,6 +104,88 @@ roberta_large.spines['right'].set_visible(False)
 roberta_large.spines['top'].set_visible(False)
 
 
+# asdiv 
+
+dense_asdiv = [78.64]
+
+results_dir = "/home/sliu/project_space/pruning_cfails/Math/gts/asdiv/"
+output_files = os.listdir(os.path.join(results_dir))
+all_methods = sorted_nicely(output_files)
+
+
+results_sdiv = []
+for method in all_methods:
+    if 'dense' not in method and 'imp' not in method:
+        print(method)
+        method_dirs = os.path.join(results_dir, str(method))
+        method_sparsity = sorted_nicely(os.listdir(method_dirs))
+
+        for sparsity in method_sparsity:
+            print(sparsity)
+            sparsity_dir = os.path.join(method_dirs, sparsity,'out', 'CV_results_cv_asdiv-a.json')
+            with open(sparsity_dir) as file:
+                for line in file:
+                    if '5-fold avg acc score' in line:
+                        results_sdiv.append(100*float(line.split()[-1][1:-2]))
+
+print(np.array(results_sdiv).reshape(10,-1))
+robert_gmp_asdiv = [77.24, 78.55, 78.23, 75.35, 69.60, 65.57, 66.56, 67.63, 67.94, 67.79]
+robert_gm_after_asdiv = [79.78, 78.96466721, 79.21117502, 68.69350863, 65.73541495, 68.44700082, 67.78964667,  67.4609696, 65.16023007, 63.92769104]
+robert_gm_before_asdiv = [78.14297453, 71.73377157,  0.82169269,  0.82169269,  0.82169269,  0.82169269, 0.82169269 , 0.82169269 , 0.82169269 , 0.82169269]
+robert_gm_rigl_asdiv = [75.51, 68.69, 0.82169269,  0.82169269,  0.82169269,  0.82169269, 0.82169269 , 0.82169269 , 0.82169269 , 0.82169269]
+
+robert_random_after_asdiv = [71.73377157, 67.4609696,  62.53081348, 63.43467543, 60.14790468, 60.55875103,
+  59.90139688, 58.75102712, 57.27198028, 58.50451931]
+
+# print(robert_random_after_asdiv)
+robert_random_before_asdiv = [48.06902219, 45.76828266, 51.68447001, 54.97124076, 59.16187346, 59.07970419,
+  58.91536565, 58.17584224, 55.87510271, 45.35743632]
+# print(robert_random_before_asdiv)
+robert_random_rigl_asdiv = [35.08627773 ,44.28923583, 49.30156122, 56.86113394, 55.05341002, 58.09367297,
+  57.60065735, 59.98356615, 56.9433032,  53.73870173]
+robert_snip_asdiv= [59.65488907, 56.20377979, 61.05176664 ,60.39441249 ,53.73870173 ,60.55875103,
+  60.47658176, 59.49055053, 59.81922761, 60.31224322 ]
+robert_snip_rigl_asdiv= [60.06573541, 61.38044371, 61.70912079, 61.13393591, 61.21610518, 60.72308956,
+  61.13393591 ,61.46261298,60.6409203 , 59.49055053 ]
+
+robert_lth_asdiv = [80.27, 79.38, 77.32, 71.90, 65.08, 59.57, 56.86, 54.15, 54.81, 52.67]
+
+
+
+x_axis = range(10)
+roberta_large = fig.add_subplot(1,3,2)
+roberta_large.plot(x_axis, dense_asdiv*10,  '-o',   label='Dense model',color='black',linewidth=linewidth, markersize=markersize, )
+
+roberta_large.plot(x_axis, robert_snip_asdiv,  '-o',   label='SNIP (Before)',color='#00FF00',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_lth_asdiv,  '-o',   label='LTH (After)',color='orange',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_snip_rigl_asdiv,  '--o',   label='SNIP+RIGL (Before)',color='#00FF00',linewidth=linewidth, markersize=markersize )
+roberta_large.plot(x_axis, robert_gm_after_asdiv,  '-o',   label='One-Shot LRR (After)',color='#00BFFF',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_random_before_asdiv,  '-o',   label='Random (Before)',color='brown',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_random_after_asdiv,  '-o',   label='Random LRR (After)',color='#0072BD',linewidth=linewidth, markersize=markersize, )
+roberta_large.plot(x_axis, robert_random_rigl_asdiv,  '--o',   label='Random+RIGL (Before)',color='brown',linewidth=linewidth, markersize=markersize)
+roberta_large.plot(x_axis, robert_gmp_asdiv,  '-o',   label='GMP (During)',color='magenta',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, robert_gm_before_asdiv,  '-o',   label='OMP (Before)' ,color='#bcbd22',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, robert_gm_rigl_asdiv,  '--o',   label='OMP+RIGL (Before)',color='#bcbd22',linewidth=linewidth, markersize=markersize )
+
+
+roberta_large.set_title('GTS on ASDiv-A',fontsize=Titlesize)
+roberta_large.set_xticks(range(10))
+# roberta_large.axes.get_xaxis().set_visible(False)
+roberta_large.set_ylabel('Accuracy [%]', fontsize=Titlesize)
+roberta_large.set_xlabel('Sparsity',fontsize=fontsize)
+roberta_large.set_xticklabels([], rotation=45,fontsize=fontsize )
+roberta_large.set_xticklabels(np.array( [0.2, 0.36, 0.488, 0.590, 0.672, 0.738, 0.791, 0.8325, 0.866, 0.893]), fontsize=10 )
+roberta_large.tick_params(axis='both', which='major', labelsize=fontsize)
+# xposition = [3,7]
+# for xc in xposition:
+#     plt.axvline(x=xc, color='#a90308', linestyle='--', alpha=0.5)
+roberta_large.grid(True, linestyle='-', linewidth=0.5, )
+
+roberta_large.spines['right'].set_visible(False)
+roberta_large.spines['top'].set_visible(False)
+
+
+
 
 # svamp
 
@@ -222,7 +224,7 @@ roberta_large.plot(x_axis, robert_gmp_svamp,  '-o', color='magenta',linewidth=li
 
 roberta_large.set_title('GTS on SVAMP',fontsize=Titlesize)
 roberta_large.set_xticks(range(10))
-roberta_large.axes.set_ylabel().set_visible(True)
+# roberta_large.axes.set_ylabel().set_visible(True)
 roberta_large.set_ylabel('Accuracy [%]', fontsize=Titlesize)
 roberta_large.set_xlabel('Sparsity',fontsize=fontsize)
 roberta_large.set_xticklabels([], rotation=45,fontsize=fontsize )
