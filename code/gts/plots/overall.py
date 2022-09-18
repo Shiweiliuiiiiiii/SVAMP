06bfe864b3b4a8048cd24bf3ed717d7c6b1ab96c
刘世伟 <liushiwei@v097211.vpn.tue.nl>
2022-09-18 14:37:33 -0500

    add gmp for gts

--- code/gts/plots/overall.py
+++ code/gts/plots/overall.py
@@ -33,6 +33,7 @@ for method in all_methods:
         method_sparsity = sorted_nicely(os.listdir(method_dirs))
 
         for sparsity in method_sparsity:
+            print(sparsity)
             sparsity_dir = os.path.join(method_dirs, sparsity,'out', 'CV_results_cv_asdiv-a.json')
             with open(sparsity_dir) as file:
                 for line in file:
