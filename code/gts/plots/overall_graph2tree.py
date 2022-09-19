cda06d6ff20aa044f0fb2ce90f7b310a45544be2
刘世伟 <liushiwei@v079183.vpn.tue.nl>
2022-09-19 13:46:52 -0500

    run the rest for graph2tree

--- code/gts/plots/overall_graph2tree.py
+++ code/gts/plots/overall_graph2tree.py
@@ -41,16 +41,16 @@ robert_lth_mawps = [87.86, 88.54, 89.47, 88.85, 87.60, 86.14, 84.68, 84.01, 82.8
 roberta_large = fig.add_subplot(1,3,1)
 roberta_large.plot(x_axis, dense_mawps*10,  '-o',color='black',linewidth=linewidth, markersize=markersize, )
 
-roberta_large.plot(x_axis, robert_snip_mawps,  '-o',color='#00FF00',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, robert_snip_mawps,  '-o',color='#00FF00',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, robert_lth_mawps,  '-o', color='orange',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_snip_rigl_mawps,  '--o',color='#00FF00',linewidth=linewidth, markersize=markersize )
+# roberta_large.plot(x_axis, robert_snip_rigl_mawps,  '--o',color='#00FF00',linewidth=linewidth, markersize=markersize )
 roberta_large.plot(x_axis, robert_omp_after_mawps,  '-o',color='#00BFFF',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, robert_random_before_mawps,  '-o',color='brown',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, robert_random_after_mawps,  '-o',color='#0072BD',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, robert_random_rigl_mawps,  '--o',color='brown',linewidth=linewidth, markersize=markersize)
 roberta_large.plot(x_axis, robert_gmp_mawps,  '-o', color='magenta',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_omp_before_mawps,  '-o' ,color='#bcbd22',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_omp_rigl_mawps,  '--o',color='#bcbd22',linewidth=linewidth, markersize=markersize )
+# roberta_large.plot(x_axis, robert_omp_before_mawps,  '-o' ,color='#bcbd22',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, robert_omp_rigl_mawps,  '--o',color='#bcbd22',linewidth=linewidth, markersize=markersize )
 
 
 roberta_large.set_title('GTS on MAWPS',fontsize=Titlesize)
@@ -130,16 +130,16 @@ dense_svamp = [43.4]
 # after is rerunning due to the issue of loading function
 # gmp is running
 robert_gmp_svamp =           [42.0, 41.1, 41.7, 40.5, 39.9, 38.8, 35.7, 33.1, 34.8, 33.6]
-robert_gm_before_svamp =     [38.5, 29.1, 1.4,  1.4,  1.4,  1.4,  1.4,  1.4,  1.4,  1.4 ]
-robert_gm_rigl_svamp   =     [37.2, 30.5, 1.4,  1.4,  1.4,  1.4,  1.4,  1.4,  1.4,  1.4 ]
-robert_snip_rigl_svamp =     [27.6, 27.5, 27.6, 26.2, 25.8, 26.0, 26.2, 25.7, 25.4, 24.8]
-robert_snip_svamp =          [26.9, 26.6, 27.4, 26.2, 26.0, 25.6, 26.4, 25.8, 25.1, 26.2]
-robert_lth_svamp =           [39.8, 40.1, 39.1, 35.7, 33.7, 25.6, 24.4, 23.6, 22.3, 21.3]
+# robert_gm_before_svamp =     [38.5, 29.1, 1.4,  1.4,  1.4,  1.4,  1.4,  1.4,  1.4,  1.4 ]
+# robert_gm_rigl_svamp   =     [37.2, 30.5, 1.4,  1.4,  1.4,  1.4,  1.4,  1.4,  1.4,  1.4 ]
+robert_snip_rigl_svamp =     [33.4, 33.9, 34.5, 33.2, 34.7, 34.2, 33.1, 34.6, 33.2, 32.4]
+robert_snip_svamp =          [33.9, 33.8, 31.0, 33.7, 33.8, 33.2, 33.9, 34.7, 33.7, 35.0]
+robert_lth_svamp =           [43.3, 41.1, 40.6, 39.9, 38.7, 33,1, 32.3, 29.3, 29.0, 27.1]
 
-robert_gm_after_svamp =      [40.9, 39.3, 39.3, 37.5, 29.3, 28.7, 29.9, 28.6, 29.1, 26.0]
-robert_random_before_svamp = [23.1, 26.2, 24.7, 27.0, 25.6, 24.6, 24.7, 24.1, 22.9, 21.9]
-robert_random_after_svamp  = [34.3, 29.6, 26.6, 26.9, 26.1, 24.5, 25.5, 23.7, 23.2, 20.6]
-robert_random_rigl_svamp =   [28.4, 21.5, 25.3, 27.5, 26.2, 24.7, 25.0, 23.3, 24.0, 22.6]
+robert_gm_after_svamp =      [44.4, 43.1, 42.7, 40.7, 35.3, 33.1, 35.1, 34.3, 34.1, 33.0]
+robert_random_before_svamp = [33.1, 27.4, 27.7, 32.2, 35.2, 32.7, 31.8, 30.7, 30.1, 26.5]
+robert_random_after_svamp  = [41.0, 36.9, 34.0, 34.1, 34.0, 33.9, 32.4, 32.1, 30.0, 30.5]
+robert_random_rigl_svamp =   [33.0, 34.0, 31.3, 33.8, 34.0, 33.1, 31.7, 30.1, 31.2, 30.6]
 
 x_axis = range(10)
 roberta_large = fig.add_subplot(1,3,3)
@@ -153,8 +153,8 @@ roberta_large.plot(x_axis, robert_random_before_svamp,  '-o',color='brown',linew
 roberta_large.plot(x_axis, robert_random_after_svamp,  '-o',color='#0072BD',linewidth=linewidth, markersize=markersize, )
 roberta_large.plot(x_axis, robert_random_rigl_svamp,  '--o',color='brown',linewidth=linewidth, markersize=markersize)
 roberta_large.plot(x_axis, robert_gmp_svamp,  '-o', color='magenta',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_gm_before_svamp,  '-o' ,color='#bcbd22',linewidth=linewidth, markersize=markersize, )
-roberta_large.plot(x_axis, robert_gm_rigl_svamp,  '--o',color='#bcbd22',linewidth=linewidth, markersize=markersize )
+# roberta_large.plot(x_axis, robert_gm_before_svamp,  '-o' ,color='#bcbd22',linewidth=linewidth, markersize=markersize, )
+# roberta_large.plot(x_axis, robert_gm_rigl_svamp,  '--o',color='#bcbd22',linewidth=linewidth, markersize=markersize )
 
 
 roberta_large.set_title('GTS on SVAMP',fontsize=Titlesize)
